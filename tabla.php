5837fd0030b772bea59b38b6b6638081e96bbb4d
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-16 19:09:22 +0200

    Refactor

--- tabla.php
+++ tabla.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Tabla de multiplicar</title>
-  </head>
-  <body>
-    <?php
-    $num = 4;
-
-    echo "<table border='1'>";
-    echo "<thead>";
-    echo "<th>$num</th><th>x</th><th>n</th>=<th>r</th>";
-    echo "<thead>";
-    echo "<tbody>";
-    for ($i=0; $i <= 10; $i++) {
-      $res = $num * $i;
-      echo "<tr><td>$num</td><td>x</td><td>$i</td><td>=</td><td>$res</td></tr>";
-    }
-    echo "<tbody>";
-    echo "</table>";
-    ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Tabla de multiplicar</title>
+</head>
+<body>
+  <?php $num = 4; ?>
+  <table border='1'>
+    <thead>
+      <th><?= $num ?></th>
+      <th>x</th>
+      <th>n</th>
+      <th>=</th>
+      <th>r</th>
+    </thead>
+    <tbody>
+      <?php for ($i=0; $i <= 10; $i++) { ?>
+        <tr>
+          <td><?= $num ?></td>
+          <td>x</td>
+          <td><?= $i ?></td>
+          <td>=</td>
+          <td><?= $num * $i ?></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
 </html>
