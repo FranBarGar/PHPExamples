f68756136e2336e728af71722baa3e4bdbfb7e34
FranciscoBarbaG <francisco.barba@iesdonana.org>
2018-10-29 19:16:36 +0100

    Filtra y prepara sentencias

--- filmAffinity/index.php
+++ filmAffinity/index.php
@@ -6,24 +6,46 @@
 </head>
 <body>
     <?php
+    $buscarTitulo = isset($_GET['buscarTitulo']) ? trim($_GET['buscarTitulo']) : '';
+
     $pdo = new PDO('pgsql:host=localhost;dbname=fa', 'fa', 'fa');
-    $numFilas = $pdo -> exec("INSERT INTO generos (genero)
-                              VALUES ('Costumbrismo')");
-    $st = $pdo->query('SELECT * FROM generos;');
+    $stPeliculas = $pdo->prepare('SELECT p.*, genero
+                                    FROM peliculas p JOIN generos g ON p.genero_id = g.id
+                                   WHERE titulo ILIKE :titulo;');
+    $stPeliculas -> execute([':titulo'=>"%$buscarTitulo%"]);
     ?>
+<div>
+    <fieldset>
+        <legend>Buscar...</legend>
+        <form action="" method="get">
+            <label for="buscarTitulo">Buscar por titulo</label>
+            <input id="buscarTitulo" type="text" name="buscarTitulo" value="<?= $buscarTitulo ?>">
+            <input type="submit" value="Buscar">
+        </form>
+    </fieldset>
+</div>
+
+<div style="margin-top: 20px">
     <table border="1" style="margin:auto">
         <thead>
-            <th>ID</th>
+            <th>Titulo</th>
+            <th>Año</th>
+            <th>Sinopsis</th>
+            <th>Duración</th>
             <th>Género</th>
         </thead>
         <tbody>
-            <?php foreach ($st as $fila) { ?>
+            <?php foreach ($stPeliculas as $fila) { ?>
                 <tr>
-                    <td><?= $fila['id'] ?></td>
+                    <td><?= $fila['titulo'] ?></td>
+                    <td><?= $fila['anyo'] ?></td>
+                    <td><?= $fila['sinopsis'] ?></td>
+                    <td><?= $fila['duracion'] ?></td>
                     <td><?= $fila['genero'] ?></td>
                 </tr>
             <?php } ?>
         </tbody>
     </table>
+</div>
 </body>
 </html>
